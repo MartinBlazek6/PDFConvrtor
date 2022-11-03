68d7db57bf6539813dc730ca7259226982de15c2
Martin Blažek <martinblazek6@gmail.com>
2022-11-04 00:30:02 +0100

    dir removed

--- src/main/java/com/PDF2WORD/demo/DemoApplication.java
+++ src/main/java/com/PDF2WORD/demo/DemoApplication.java
@@ -1,11 +1,9 @@
 package com.PDF2WORD.demo;
 
-import com.PDF2WORD.demo.controller.UploadController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.swing.*;
-import java.io.File;
 
 @SpringBootApplication
 public class DemoApplication extends JFrame {
--- src/main/java/com/PDF2WORD/demo/controller/UploadController.java
+++ src/main/java/com/PDF2WORD/demo/controller/UploadController.java
@@ -1,22 +1,14 @@
 package com.PDF2WORD.demo.controller;
 
 import com.PDF2WORD.demo.service.ConverterService;
-import com.spire.pdf.FileFormat;
-import com.spire.pdf.PdfDocument;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
 
 @Controller
