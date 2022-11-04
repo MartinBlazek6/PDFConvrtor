4c441174ca49e3b062bcf7a10753c3af5c678a53
Martin Blažek <martinblazek6@gmail.com>
2022-11-04 01:16:12 +0100

    spire removed

--- src/main/java/com/PDF2WORD/demo/service/ConvertererviceImpl.java
+++ src/main/java/com/PDF2WORD/demo/service/ConvertererviceImpl.java
@@ -1,7 +1,7 @@
 package com.PDF2WORD.demo.service;
 
-//import com.spire.pdf.FileFormat;
-//import com.spire.pdf.PdfDocument;
+import com.spire.pdf.FileFormat;
+import com.spire.pdf.PdfDocument;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,15 +15,15 @@ public class ConvertererviceImpl implements ConverterService{
 
     @Override
     public void convertFileToPDF(MultipartFile file) {
-//        try {
-//            Path fileNameAndPath = Paths.get(uploadDirectory, file.getOriginalFilename());
-//            PdfDocument pw=new PdfDocument();
-//            pw.loadFromBytes(file.getBytes());
-//            pw.saveToFile(String.valueOf(fileNameAndPath).replace("pdf","docx"), FileFormat.DOCX);
-//            pw.close();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
+        try {
+            Path fileNameAndPath = Paths.get(uploadDirectory, file.getOriginalFilename());
+            PdfDocument pw=new PdfDocument();
+            pw.loadFromBytes(file.getBytes());
+            pw.saveToFile(String.valueOf(fileNameAndPath).replace("pdf","docx"), FileFormat.DOCX);
+            pw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
